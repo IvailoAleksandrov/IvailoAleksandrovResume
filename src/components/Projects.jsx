5c6dc9077eaa74d8297d02555225e11117d2e1d0
IvailoAleksandrov <ivailoaleksandrov123@gmail.com>
2024-06-03 16:05:21 +0300

    Rounded carousel modify

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import movieCollection from '../Images/movie.png'
-import shoppinglist from '../Images/shopping.png'
-import react from '../Images/react.png'
-
+import movieCollection from '../Images/movie.png';
+import shoppinglist from '../Images/shopping.png';
+import react from '../Images/react.png';
 
 const Projects = () => {
   const scrollToSection = (sectionId) => {
@@ -14,51 +13,51 @@ const Projects = () => {
 
   return (
     <>
-    <h3 className='custom-margin' id='projects' >Projects</h3>
-    <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    <a href="https://moviecollection1.netlify.app/">
-            <img src={movieCollection} className="d-block w-100" alt="Movie collection app" />
-          </a>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Movie collection App</h5>
-        <p>Movie Library.</p>
+      <h3 className='custom-margin' id='projects'>Projects</h3>
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <a href="https://moviecollection1.netlify.app/">
+              <img src={movieCollection} className="d-block w-100 rounded" alt="Movie collection app" />
+            </a>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Movie collection App</h5>
+              <p>Movie Library.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <a href="https://shoppinglistttt.netlify.app/">
+              <img src={shoppinglist} className="d-block w-100 rounded" alt="Shopping list app" />
+            </a>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Shopping list App</h5>
+              <p>Vanilla JavaScript App.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <a href="https://reactcoreconcepts.netlify.app">
+              <img src={react} className="d-block w-100 rounded" alt="React concepts" />
+            </a>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>React concepts</h5>
+              <p>Basic React Application - Components, JSX, Props, States and more.</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-    </div>
-    <div class="carousel-item">
-    <a href="https://shoppinglistttt.netlify.app/">
-            <img src={shoppinglist} className="d-block w-100" alt="Shopping list app" />
-          </a>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Shopping list App</h5>
-        <p>Vanilla JavaScript App.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-    <a href="https://reactcoreconcepts.netlify.app">
-            <img src={react} className="d-block w-100" alt="React concepts" />
-          </a>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>React concepts</h5>
-        <p>Basic React Application - Components, JSX, Props, States and more.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
     </>
   );
 };
